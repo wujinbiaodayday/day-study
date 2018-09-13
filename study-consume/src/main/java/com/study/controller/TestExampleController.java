ef4a5dd0b725880f18cf0aaa6dc97655d9bb46f2
wu <jinbiao.wu@daydaycook.com>
2018-09-13 20:29:09 +0800

    add file

--- study-consume/src/main/java/com/study/controller/TestExampleController.java
+++ study-consume/src/main/java/com/study/controller/TestExampleController.java
@@ -1,12 +1,13 @@
 package com.study.controller;
 
+import com.alibaba.dubbo.common.utils.StringUtils;
 import com.study.api.service.TestExampleApi;
+import com.study.util.Constants;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestExampleController {
@@ -14,9 +15,42 @@ public class TestExampleController {
     @Autowired
     private TestExampleApi testExampleApi;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @ApiOperation(value="测试用例", notes = "测试用例")
     @RequestMapping(value = "/testExample/{name}", method = RequestMethod.GET)
     public String hello(@PathVariable String name){
         return testExampleApi.getTest(name);
     }
+
+    @ApiOperation(value = "redis测试", notes = "redis测试")
+    @RequestMapping(value = "/testRedis/{name}", method = RequestMethod.GET)
+    public String testRedis(@PathVariable String name) {
+        String showName = stringRedisTemplate.opsForValue().get(Constants.REDIS_EXAMPLE);
+        if (StringUtils.isBlank(showName)) {
+            showName = testExampleApi.getTest(name) + stringRedisTemplate.toString();
+            stringRedisTemplate.opsForValue().set(Constants.REDIS_EXAMPLE, showName);
+        }
+//        stringRedisTemplate.delete(Constants.REDIS_EXAMPLE);
+        return showName;
+    }
+
+//    @ApiOperation(value = "redis测试", notes = "redis测试")
+//    @RequestMapping(value = "/testRedis?{name}", method = RequestMethod.GET)
+//    public String testRedis(@RequestParam String name) {
+//        String showName = stringRedisTemplate.opsForValue().get(Constants.REDIS_EXAMPLE);
+//        if (StringUtils.isBlank(showName)) {
+//            showName = testExampleApi.getTest(name) + stringRedisTemplate.toString();
+//            stringRedisTemplate.opsForValue().set(Constants.REDIS_EXAMPLE, showName);
+//        }
+////        stringRedisTemplate.delete(Constants.REDIS_EXAMPLE);
+//        return showName;
+//    }
+
+
+
 }
---
+++ study-consume/src/main/java/com/study/util/Constants.java
@@ -0,0 +1,6 @@
+package com.study.util;
+
+public class Constants {
+
+    public static String REDIS_EXAMPLE = "example";
+}
