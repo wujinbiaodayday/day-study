62ee5d65a60cb3526ca60b4ed69c99070e18b165
wu <jinbiao.wu@daydaycook.com>
2018-09-13 20:47:53 +0800

    add file

--- study-consume/src/main/java/com/study/controller/TestExampleController.java
+++ study-consume/src/main/java/com/study/controller/TestExampleController.java
@@ -1,13 +1,19 @@
 package com.study.controller;
 
+import com.alibaba.dubbo.common.json.JSON;
 import com.alibaba.dubbo.common.utils.StringUtils;
 import com.study.api.service.TestExampleApi;
+import com.study.model.UserModel;
 import com.study.util.Constants;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TestExampleController {
@@ -39,17 +45,49 @@ public class TestExampleController {
         return showName;
     }
 
-//    @ApiOperation(value = "redis测试", notes = "redis测试")
-//    @RequestMapping(value = "/testRedis?{name}", method = RequestMethod.GET)
-//    public String testRedis(@RequestParam String name) {
-//        String showName = stringRedisTemplate.opsForValue().get(Constants.REDIS_EXAMPLE);
-//        if (StringUtils.isBlank(showName)) {
-//            showName = testExampleApi.getTest(name) + stringRedisTemplate.toString();
-//            stringRedisTemplate.opsForValue().set(Constants.REDIS_EXAMPLE, showName);
-//        }
-////        stringRedisTemplate.delete(Constants.REDIS_EXAMPLE);
-//        return showName;
-//    }
+
+    /**
+     * 1 无参数
+     */
+    @RequestMapping(value = "/searchList", method = RequestMethod.GET)
+    public List<UserModel> searchList() {
+        List<UserModel> list = new ArrayList();
+        return list;
+    }
+
+    /**
+     * 2 查询字符串参数，可选和必选参数2. 查询字符串参数，可选和必选参数
+     */
+    @RequestMapping(value="/listByName",method = RequestMethod.GET)
+    public String listByName(@RequestParam(value = "name", required = true) String name) throws IOException {
+        UserModel userModel = new UserModel();
+        userModel.setName(name);
+        return JSON.json(userModel);
+    }
+
+    /**
+     * 3 json参数，RestController用实体类型接受
+     */
+    @RequestMapping(value="/addUser",method = RequestMethod.GET)
+    public String addUser(@RequestBody UserModel userModel) throws IOException {
+        return JSON.json(userModel);
+    }
+
+    /**
+     * 4 路径参数
+     */
+    @RequestMapping(value="/searchListById/{name}",method = RequestMethod.GET)
+    public String searchListById(@PathVariable String name) {
+        return name;
+    }
+
+
+
+
+
+
+
+
 
 
 
---
+++ study-consume/src/main/java/com/study/model/UserModel.java
@@ -0,0 +1,32 @@
+package com.study.model;
+
+public class UserModel {
+
+    private String name;
+
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
