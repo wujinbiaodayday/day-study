a2a9c6da1090cb4b232fee3f09092fd3b41d1a5e
wu <jinbiao.wu@daydaycook.com>
2018-09-17 10:51:14 +0800

    add file

--- study-consume/src/main/java/com/study/controller/UserController.java
+++ study-consume/src/main/java/com/study/controller/UserController.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
- * user restfull
+ * 用户的业务
  */
 @Controller
 @RequestMapping(value = "/user")
@@ -15,13 +17,24 @@ public class UserController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(value = "/index")
-    public String index() {
-        return "user/index";
+    /**
+     * 登陆页面视图
+     */
+    public String LOGIN_PAGE_VIEW = "/user/loginPage";
+
+    /**
+     * 登陆页面
+     * @return
+     */
+    @RequestMapping(value = "/loginPage", method = RequestMethod.GET)
+    public ModelAndView loginPage() {
+        try {
+            ModelAndView view = new ModelAndView(LOGIN_PAGE_VIEW);
+            return view;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return null;
     }
-
-
-
-
 
 }
